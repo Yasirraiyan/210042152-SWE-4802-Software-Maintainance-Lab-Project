ed303b391bc86df328f3cc61490d13755a836f41
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2026-08-16 11:43:49 +0600

    Create command.py

---
+++ Commands/command.py
@@ -0,0 +1,19 @@
+1.cd C:\Users\ASUS\Desktop\Math-Problem-Solver
+2.python -m venv venv
+3..\venv\Scripts\Activate.ps1
+4.pip install flask sympy
+5.pip install coverage radon snakeviz viztracer black yapf
+6.python -m unittest discover -s tests -p "test_*.py"
+    |
+    |
+   |................
+  Output
+----------------------------------------------------------------------
+Ran 16 tests
+
+OK
+
+7.python main.py
+8.python app.py
+9.http://127.0.0.1:5000
+10.C:\Users\ASUS\Desktop\Math-Problem-Solver
