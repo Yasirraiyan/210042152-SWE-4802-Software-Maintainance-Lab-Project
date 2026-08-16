24d1138cfa57a4c959f85052e1ed341f18736b9b
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2026-08-16 12:18:12 +0600

    Update command.py

--- Commands/command.py
+++ Commands/command.py
@@ -17,3 +17,13 @@ OK
 8)python app.py
 9)http://127.0.0.1:5000
 10)C:\Users\ASUS\Desktop\Math-Problem-Solver
+11.)cd C:\Users\ASUS\Desktop\Math-Problem-Solver
+12.).\venv\Scripts\Activate.ps1
+13.)pip install loguru pysnooper viztracer snakeviz
+14.)python -m analysis.logging_demo
+15.)dir analysis
+16.)python -m analysis.pysnooper_demo
+17.)viztracer main.py
+18.)vizviewer result.json
+19.)python -m cProfile -o profile.prof main.py
+20.)snakeviz profile.prof
